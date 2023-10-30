d4669239be2420dc739a9c05fe9b2d73969f8a10
Sheldon <kasebrot@live.com>
2023-10-30 15:10:49 -0500

    Updated the delete UI to look like a button.

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ const App = () => {
     const deleteNote = async({ id }) => {
         const index = state.notes.findIndex(n => n.id === id)
         const notes = [
-            ...state.notes.slice(0, index),
+            ...state.notes.slice(0, index), // Look into filter...
             ...state.notes.slice(index + 1)];
         dispatch({ type: 'SET_NOTES', notes })
         try {
@@ -115,7 +115,16 @@ const App = () => {
             <List.Item 
                 style={styles.item}
                 actions={[
-                    <p style={styles.p} onClick={() => deleteNote(item)}>Delete</p>
+                    <p style={{
+                        ...styles.p
+                        , cursor: 'pointer'
+                        , display: 'inline-block'
+                        , padding: '10px 20px'
+                        , background: 'red'
+                        , color: '#fff'
+                        , borderRadius: '5px'
+                    }} 
+                    onClick={() => deleteNote(item)}>Delete</p>
                 ]}>
             <List.Item.Meta
                 title={item.name}
